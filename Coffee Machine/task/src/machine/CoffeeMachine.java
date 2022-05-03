74f9ea6dcbe2e5cc242076f4d969d8bd2ee085d8
Vadim Colcev <vadimkolchev@gmail.com>
2022-05-03 16:53:33 +0300

    OOP based release

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,54 +1,52 @@
 package machine;
 
-import java.util.Scanner;
+import static machine.MachineState.*;
 
 public class CoffeeMachine {
 
-    static Scanner scanner = new Scanner(System.in);
+    int water;
+    int milk;
+    int beans;
+    int cups;
+    int cash;
+    MachineState state;
 
-    // supplies of coffee machine
-    static int CASH_IN_MACHINE = 550;
-    static int WATER_IN_MACHINE = 400;
-    static int MILK_IN_MACHINE = 540;
-    static int BEANS_IN_MACHINE = 120;
-    static int CUPS_IN_MACHINE = 9;
-
-    // state of machine
-    static boolean IS_RUNNING = true;
-
-    public static void main(String[] args) {
-
-        // run the machine until user wants to exit
-        while (IS_RUNNING) {
-            runMachine();
-        }
-
+    public CoffeeMachine(int water, int milk, int beans, int cups, int cash) {
+        this.water = water;
+        this.milk = milk;
+        this.beans = beans;
+        this.cups = cups;
+        this.cash = cash;
+        this.state = MachineState.SHOW_MENU;
     }
 
-    private static void printMachineState() {
+    void showMainMenu() {
+        System.out.println("Write action (buy, fill, take, remaining, exit):");
+    }
+
+    void showCoffeeChoiceMenu() {
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
+    }
+
+    void printMachineState() {
         /* gets the state from class variables and outputs it in formatted way */
         System.out.println("The coffee machine has:");
-        System.out.printf("%d ml of water\n", WATER_IN_MACHINE);
-        System.out.printf("%d ml of milk\n", MILK_IN_MACHINE);
-        System.out.printf("%d g of coffee beans\n", BEANS_IN_MACHINE);
-        System.out.printf("%d disposable cups\n", CUPS_IN_MACHINE);
-        System.out.printf("$%d of money\n", CASH_IN_MACHINE);
+        System.out.printf("%d ml of water\n", this.water);
+        System.out.printf("%d ml of milk\n", this.milk);
+        System.out.printf("%d g of coffee beans\n", this.beans);
+        System.out.printf("%d disposable cups\n", this.cups);
+        System.out.printf("$%d of money\n", this.cash);
         System.out.println();
     }
 
-    private static void runMachine() {
-        /* run machine and let user choose the action according to operation he needs to do */
-        IS_RUNNING = true;
-        System.out.println("Write action (buy, fill, take, remaining, exit):");
-        String action = scanner.next();
-        System.out.println();
+    void runMachine(String input) {
 
-        switch (action.toLowerCase()) {
+        switch (input.toLowerCase()) {
             case "buy":
-                buyCoffee();
+                this.state = BUY_COFFEE;
                 break;
             case "fill":
-                fillMachine();
+                this.state = FILL_WATER;
                 break;
             case "take":
                 takeMoneyFromMachine();
@@ -57,7 +55,7 @@ public class CoffeeMachine {
                 printMachineState();
                 break;
             case "exit":
-                IS_RUNNING = false;
+                this.state = STOPPED;
                 break;
             default:
                 System.out.println("Unknown command");
@@ -66,152 +64,77 @@ public class CoffeeMachine {
 
     }
 
-    private static void buyCoffee() {
+    void buyCoffee(String input) {
         /* Allows to purchase different flavors of coffee and changes state of coffee machine accordingly */
-
-        // required resources for coffee of each type
-        int espressoWater = 250;
-        int espressoMilk = 0;
-        int espressoBeans = 16;
-        int espressoCost = 4;
-
-        int latteWater = 350;
-        int latteMilk = 75;
-        int latteBeans = 20;
-        int latteCost = 7;
-
-        int cappuccinoWater = 200;
-        int cappuccinoMilk = 100;
-        int cappuccinoBeans = 12;
-        int cappuccinoCost = 6;
-        int choice;
-
-        // ask user to choose coffee flavor
-        while (true) {
-            System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
-            if (scanner.hasNextInt()) {
-                choice = scanner.nextInt();
-            } else {
-                String input = scanner.next();
-                if ("back".equals(input)) {
-                    System.out.println("Unknown input");
-                    return;
-                }
-                continue;
-            }
-            break;
-        }
+        Coffee espresso = new Coffee(250, 0, 14, 4);
+        Coffee latte = new Coffee(350, 75, 20, 7);
+        Coffee cappuccino = new Coffee(200, 100, 12, 6);
 
 
         // change state of coffee machine according to user coffee type choice
-        switch (choice) {
-            case 1:
-                updateMachineState(espressoWater, espressoMilk, espressoBeans, espressoCost);
+        switch (input.toLowerCase()) {
+            case "1":
+                updateMachineState(espresso.water, espresso.milk, espresso.beans, espresso.cost);
                 break;
-            case 2:
-                updateMachineState(latteWater, latteMilk, latteBeans, latteCost);
+            case "2":
+                updateMachineState(latte.water, latte.milk, latte.beans, latte.cost);
                 break;
-            case 3:
-                updateMachineState(cappuccinoWater, cappuccinoMilk, cappuccinoBeans, cappuccinoCost);
+            case "3":
+                updateMachineState(cappuccino.water, cappuccino.milk, cappuccino.beans, cappuccino.cost);
+                break;
+            case "back":
                 break;
             default:
                 System.out.println("No such coffee type");
                 break;
         }
+
+        this.state = MachineState.SHOW_MENU;
     }
 
-    private static void fillMachine() {
-        /* Asks user how many assets he wants to replenish and change the state of machine accordingly */
-        int waterToAdd;
-        int beansToAdd;
-        int cupsToAdd;
-        int milkToAdd;
-
-        // get user input
-        while (true) {
-            System.out.println("Write how many ml of water you want to add:");
-            if (scanner.hasNextInt()) {
-                waterToAdd = scanner.nextInt();
-            } else {
-                System.out.println("You should enter an integer number.");
-                scanner.next();
-                continue;
-            }
-            break;
-        }
-
-        while (true) {
-
-            System.out.println("Write how many ml of milk you want to add:");
-
-            if (scanner.hasNextInt()) {
-                milkToAdd = scanner.nextInt();
-            } else {
-                System.out.println("You should enter an integer number.");
-                scanner.next();
-                continue;
-            }
-            break;
-        }
-
-        while (true) {
-            System.out.println("Write how many grams of coffee beans you want to add:");
-
-            if (scanner.hasNextInt()) {
-                beansToAdd = scanner.nextInt();
-            } else {
-                System.out.println("You should enter an integer number.");
-                scanner.next();
-                continue;
-            }
-            break;
-        }
-
-        while (true) {
-            System.out.println("Write how many disposable cups of coffee you want to add:");
-
-            if (scanner.hasNextInt()) {
-                cupsToAdd = scanner.nextInt();
-            } else {
-                System.out.println("You should enter an integer number.");
-                scanner.next();
-                continue;
-            }
-            break;
-        }
-
-        System.out.println();
-
-        // replenish the machine
-        WATER_IN_MACHINE += waterToAdd;
-        MILK_IN_MACHINE += milkToAdd;
-        BEANS_IN_MACHINE += beansToAdd;
-        CUPS_IN_MACHINE += cupsToAdd;
+    void addWater(int input) {
+        this.water += input;
+        this.state = FILL_MILK;
     }
 
+    void addMilk(int input) {
+        this.milk += input;
+        this.state = FILL_BEANS;
+    }
 
-    private static void takeMoneyFromMachine() {
+    void addBeans(int input) {
+        this.beans += input;
+        this.state = FILL_CUPS;
+    }
+
+    void addCups(int input) {
+        this.cups += input;
+        this.state = SHOW_MENU;
+    }
+
+    void takeMoneyFromMachine() {
         /* take all the money from machine and change the state of machine accordingly */
         // remove cash from machine
-        System.out.printf("I gave you $%d\n", CASH_IN_MACHINE);
+        System.out.printf("I gave you $%d\n", this.cash);
         System.out.println();
-        CASH_IN_MACHINE = 0;
+        this.cash = 0;
+        this.state = SHOW_MENU;
     }
 
-    private static boolean hasSufficientResources(int water, int milk, int beans) {
-        if (water > WATER_IN_MACHINE) {
+    private boolean hasSufficientResources(int water, int milk, int beans) {
+        if (water > this.water) {
             System.out.println("Sorry, not enough water!");
             System.out.println();
             return false;
-        } else if (milk > MILK_IN_MACHINE) {
+        } else if (milk > this.milk) {
             System.out.println("Sorry, not enough milk!");
             System.out.println();
             return false;
-        } else if (beans > BEANS_IN_MACHINE) {
+        } else if (beans > this.beans) {
             System.out.println("Sorry, not enough beans!");
             System.out.println();
             return false;
-        } else if (CUPS_IN_MACHINE == 0) {
+        } else if (this.cups == 0) {
             System.out.println("Sorry, not enough caps!");
             System.out.println();
             return false;
@@ -222,15 +145,17 @@ public class CoffeeMachine {
         }
     }
 
-    private static void updateMachineState(int water, int milk, int beans, int cost) {
+    private void updateMachineState(int water, int milk, int beans, int cost) {
         if (hasSufficientResources(water, milk, beans)) {
-            WATER_IN_MACHINE -= water;
-            MILK_IN_MACHINE -= milk;
-            BEANS_IN_MACHINE -= beans;
-            CUPS_IN_MACHINE--;
-            CASH_IN_MACHINE += cost;
+            this.water -= water;
+            this.milk -= milk;
+            this.beans -= beans;
+            this.cups--;
+            this.cash += cost;
         }
     }
+}
 
-
+enum MachineState {
+    SHOW_MENU, BUY_COFFEE, FILL_WATER, FILL_MILK, FILL_BEANS, FILL_CUPS, TAKE, REMAINING, STOPPED,
 }
